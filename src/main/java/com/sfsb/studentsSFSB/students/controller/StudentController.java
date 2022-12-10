7d8697a474fdce89c4fa35c0e769c9a1556b3c53
aubincris <aub1wanche@gmail.com>
2022-12-10 22:18:15 +0100

    modified student controller

--- src/main/java/com/sfsb/studentsSFSB/students/controller/StudentController.java
+++ src/main/java/com/sfsb/studentsSFSB/students/controller/StudentController.java
@@ -1,16 +1,39 @@
 package com.sfsb.studentsSFSB.students.controller;
 
+import com.sfsb.studentsSFSB.students.model.Students;
+import com.sfsb.studentsSFSB.students.service.StudentsServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
+    private StudentsServiceImpl service;
+
+    public StudentController(StudentsServiceImpl service) {
+        this.service = service;
+    }
 
     @GetMapping("")
     public String showHomePage(){
         return "index";
     }
 
+    @GetMapping("/students/new")
+    public String showForm(){
+        return "form";
+
+    }
+
+    @GetMapping("/students")
+    public String showStudentsList(Model model){
+        List<Students> listStudents =  service.findAll();
+        model.addAttribute("listStudents", listStudents);
+
+        return "students";
+    }
 
 
 
