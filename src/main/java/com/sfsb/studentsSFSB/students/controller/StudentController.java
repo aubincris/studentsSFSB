5344e1315e3296230ab1e4304abad8438d7ef4df
aubincris <aub1wanche@gmail.com>
2023-01-31 03:25:45 +0100

    Added function to handle generation of PDF report

--- src/main/java/com/sfsb/studentsSFSB/students/controller/StudentController.java
+++ src/main/java/com/sfsb/studentsSFSB/students/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.sfsb.studentsSFSB.students.controller;
 
+import com.lowagie.text.DocumentException;
+import com.sfsb.studentsSFSB.students.model.GenerateReportPDF;
 import com.sfsb.studentsSFSB.students.model.Students;
 import com.sfsb.studentsSFSB.students.service.StudentsServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -46,6 +51,24 @@ public class StudentController {
         return "redirect:/students";
 
     }
+   @GetMapping("/students/delete/{id}")
+   public String deleteStudent(@PathVariable("id") Integer id){
+        service.deleteById(id);
+       return "redirect:/students";
+   }
+   @GetMapping("/students/report")
+   public void generateReportPDF(HttpServletResponse response) throws IOException, DocumentException {
+        response.setContentType("application/pdf");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=report.pdf";
+
+        response.setHeader(headerKey, headerValue);
+
+        List<Students> listStudents = service.findAll();
+       GenerateReportPDF report = new GenerateReportPDF(listStudents);
+       report.export(response);
+
+   }
 
 
 
